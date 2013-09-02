3349b3135175159a97aa238ce0b43cb38edcfe17
Brion Vibber <brion@pobox.com>
2013-09-01 18:57:09 -0700

    initial stub for windows phone 8

---
+++ HybridHostWindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HybridHostWindowsPhone.Resources;
+
+namespace HybridHostWindowsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
